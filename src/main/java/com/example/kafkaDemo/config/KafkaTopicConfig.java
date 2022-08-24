1e2462f5c12c7ee90a7e9aedb7f7744f91dd89eb
Simran Khanna <simrankhanna0.17@gmail.com>
2022-08-24 18:40:45 +0530

    Create KafkaTopicConfig.java

---
+++ src/main/java/com/example/kafkaDemo/config/KafkaTopicConfig.java
@@ -0,0 +1,16 @@
+package com.example.kafkaDemo.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig {
+
+    @Bean
+    public NewTopic sampleTopic() {
+        return TopicBuilder.name("testTopic")
+                .build();
+    }
+}
